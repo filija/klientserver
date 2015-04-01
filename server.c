cf2822174962942b73b64d7f797dcca316fdec80
filija <filija.jakub@gmail.com>
2015-04-01 21:53:04 +0200

    edit server

--- server.c
+++ server.c
@@ -14,122 +14,154 @@
 
 enum numEcode
 {
-	EARGS=0,
-	ESOCK,
-	EBIND,
-	CONNECTOK, //pripojeni se zdarilo
-	ECONNECT,
-	ELISTEN,
-	EACCEPT,
+  EARGS=0,
+  ESOCK,
+  EBIND,
+  CONNECTOK, //pripojeni se zdarilo
+  ECONNECT,
+  ELISTEN,
+  EACCEPT,
+  EREAD,
+  EWRITE,
+  EPRINT,
+  ECLOSE,
 };
 
 const char *errcodes[]={
-	"Chybne argumenty",	//EARGS
-	"Nepodarilo se vytvorit socket", //ESOCK
-	"Nepodarilo se navazat spojeni", //EBIND
-	"", //CONNECTOK
-	"Nelze se pripojit!\n", //ECONNECT
-	"Naslouchani se nezdarilo", //ELISTEN
-	"Selhalo prijmani pripojeni", //EACCEPT
+  "Chybne argumenty", //EARGS
+  "Nepodarilo se vytvorit socket", //ESOCK
+  "Nepodarilo se navazat spojeni", //EBIND
+  "", //CONNECTOK
+  "Nelze se pripojit!\n", //ECONNECT
+  "Naslouchani se nezdarilo", //ELISTEN
+  "Selhalo prijmani pripojeni", //EACCEPT
+  "Cteni selhalo", //EREAD
+  "Zapis selhal", //EWRITE
+  "Pripojeni se nezdarilo uzavrit", //ECLOSE
 };
 
 int printError(int numErr) //tisk chyb
 {
-	fprintf(stderr, "%s\n", errcodes[numErr]);
-	return numErr;
+  fprintf(stderr, "%s\n", errcodes[numErr]);
+  return -1;
 }
 
 int getParams(int argc, char **argv)
 {
-	int port;
-	if(argc!=3)
-	{
-		return printError(EARGS);				
-	}
+  int port;
+  if(argc!=3)
+  {
+    return printError(EARGS);       
+  }
 
-	else{
-			if(strcmp(argv[1], "-p")==0)
-			{
-				port=atoi(argv[2]);
-				if(port>=MINPORT && port<=MAXPORT)
-				{
-					return port;	
-				}
+  else{
+      if(strcmp(argv[1], "-p")==0)
+      {
+        port=atoi(argv[2]);
+        if(port>=MINPORT && port<=MAXPORT)
+        {
+          return port;  
+        }
 
-				else{
-					return printError(EARGS);				
-				}
-				
-			}
+        else{
+          return printError(EARGS);       
+        }
+        
+      }
 
-			else{
-				return printError(EARGS);				
-			}
-	}
-		return EARGS;
+      else{
+        return printError(EARGS);       
+      }
+  }
+    return EARGS;
 }
 
 int connectServer(int port, int *sock, struct sockaddr_in *server)  //vytvoreni socketu
 {
-	int s;
-	struct sockaddr_in sin;
-	if((s = socket(PF_INET, SOCK_STREAM, 0)) < 0)
-	{
-		printError(ESOCK);
-	}
-	  
-	  sin.sin_family = PF_INET;
-	  sin.sin_port = htons(port);
-	  sin.sin_addr.s_addr = INADDR_ANY;
+  int s;
+  struct sockaddr_in sin;
+  if((s = socket(PF_INET, SOCK_STREAM, 0)) < 0)
+  {
+    printError(ESOCK);
+  }
+    
+    sin.sin_family = PF_INET;
+    sin.sin_port = htons(port);
+    sin.sin_addr.s_addr = INADDR_ANY;
 
-	  if(bind(s, (struct sockaddr *)&sin, sizeof(sin)) <0)
-	  {
-	  	return printError(EBIND);
-	  }
+    if(bind(s, (struct sockaddr *)&sin, sizeof(sin)) <0)
+    {
+      return printError(EBIND);
+    }
 
-	  if(listen(s, 5))
-	  {
-	  	return printError(ELISTEN);
-	  }
-	  
-	  *sock=s;
-	  *server=sin;
-	  return CONNECTOK;
+    if(listen(s, 5))
+    {
+      return printError(ELISTEN);
+    }
+    
+    *sock=s;
+    *server=sin;
+    return CONNECTOK;
 }
 
 int main(int argc, char **argv)
 {
-	int sinlen, t, sock, port; 
-	struct sockaddr_in sin;
+  int sinlen, t, sock, port; 
+  struct sockaddr_in sin;
+  int j;
+  struct hostent * hp;
+  char msg[80];
 
-	if((port=getParams(argc, argv))!=EARGS)
-	{
-		printf("cislo portu je: %d\n", port);
-		if(connectServer(port, &sock, &sin)==CONNECTOK)
-		{
-			printf("pripojeni se zdarilo\n");
-    		sinlen=sizeof(sin);
+  if((port=getParams(argc, argv))!=EARGS)
+  {
+    printf("cislo portu je: %d\n", port);
+    if(connectServer(port, &sock, &sin)==CONNECTOK)
+    {
+      printf("pripojeni se zdarilo\n");
+        sinlen=sizeof(sin);
 
-    		while(1)
-    		{	//prijmani noveho pripojeni od klienta
-    			if ( (t = accept(sock, (struct sockaddr *) &sin, &sinlen) ) < 0 ) 
-    			{
-     				 	return printError(EACCEPT);
-    			}	
+        while(1)
+        { //prijmani noveho pripojeni od klienta
+          if ( (t = accept(sock, (struct sockaddr *) &sin, &sinlen) ) < 0 ) 
+          {
+              return printError(EACCEPT);
+          } 
 
-    		}
-    		return 0;	
-		}
+          hp=(struct hostent *)gethostbyaddr((char *)&sin.sin_addr,4,AF_INET);
+          j=(int)(hp->h_length);
+          printf( "From %s (%s) :%d.\n",inet_ntoa(sin.sin_addr), hp->h_name, ntohs(sin.sin_port) );
+          bzero(msg,sizeof(msg));
+          if ( read(t, msg, sizeof(msg) ) <0) 
+          { 
+              return printError(EREAD);
+          }
+          printf("length of message is %d\nmessage from client is: %s\n",strlen(msg),msg);
+          if ( write(t, msg, strlen(msg) ) < 0 ) 
+          { 
+                return printError(EWRITE);
+          } 
 
-		else{
-			return printError(ECONNECT);
-		}
-		
-    	
-	}
+          if(close(t)<0)
+          {
+            return printError(ECLOSE);
+          }
 
-	else{
-		return 1;
-	}
- 	
+        }
+        return 0; 
+    }
+
+    if(close(sock)<0)
+    {
+      return printError(ECLOSE);
+    }
+    else{
+      return printError(ECONNECT);
+    }
+       
+  }
+
+  else{
+    return 1;
+  }
+  
 }
