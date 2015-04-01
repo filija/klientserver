450603b272765174863e71d54fdd4dd448f42550
filija <filija.jakub@gmail.com>
2015-04-01 10:31:07 +0200

    add getparams

--- server.c
+++ server.c
@@ -1,7 +1,65 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAXPORT 65535
+#define MINPORT 1024
+
+enum numEcode
+{
+	EARGS=0,
+
+};
+
+const char *errcodes[]={
+	"Chybne argumenty",	//EARGS
+};
+
+int getParams(int argc, char **argv)
+{
+	int port;
+	if(argc!=3)
+	{
+		fprintf(stderr, "%s\n", errcodes[EARGS]);
+			return EARGS;
+	}
+
+	else{
+			if(strcmp(argv[1], "-p")==0)
+			{
+				port=atoi(argv[2]);
+				if(port>=MINPORT && port<=MAXPORT)
+				{
+					return port;	
+				}
+
+				else{
+					fprintf(stderr, "%s\n", errcodes[EARGS]);
+					return EARGS;				
+				}
+				
+			}
+
+			else{
+				fprintf(stderr, "%s\n", errcodes[EARGS]);
+				return EARGS;
+			}
+	}
+		return EARGS;
+}
 
 int main(int argc, char **argv)
 {
+	int port; //cislo portu
+	if((port=getParams(argc, argv))!=EARGS)
+	{
+		printf("cislo portu je: %d\n", port);
+    	return 0;	
+	}
 
-	return;
+	else{
+		return 1;
+	}
+ 	
 }
